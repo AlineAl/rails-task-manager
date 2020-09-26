e3a9abf0b7da63bb3a32b0844bf699f1d033f745
aline leroy <aline.leroy.27@gmail.com>
2020-09-26 17:21:11 +0200

    Exercice not solved yet, add front with bootstrap and boolean

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,49 @@
 class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all
+  end
+
+  def find
+    take_id
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+
+    redirect_to task_path(@task)
+  end
+
+  def edit
+    take_id
+  end
+
+  def update
+    @task = take_id
+    @task.update(task_params)
+
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task = take_id
+    @task.destroy 
+
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
+
+  def take_id
+    @task = Task.find(params[:id])
+  end
 end
